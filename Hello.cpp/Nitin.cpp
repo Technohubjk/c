0501290670e339b6c814928252ec4df1d20a4536
shivkumar <sosshivam65@gmail.com>
2022-02-28 14:50:30 +0530

    sqre,cube

---
+++ Hello.cpp/Nitin.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,sqre,cube;
+    printf("Enter Number :");
+    scanf("%d",&n);
+    sqre=n*n;
+    cube=n*n*n;
+    printf("\n sqre :%d\ncube :%d",sqre,cube);
+    return 0;
+}
