6aba3ee1600aa0f41bf13da102c063e37e77cfed
shivkumar <sosshivam65@gmail.com>
2022-03-04 15:24:20 +0530

    c language

---
+++ Hello.cpp/user.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=4;
+    int b=7;
+    float c=8.5;
+    char d='u';
+    printf("Enter the value of a is %d\n",a);
+    printf("Enter the value of b is %d\n",b);
+    printf("Enter the value of c is %f\n",c);
+    printf("Enter the vlaue the value of %c\n",d); 
+    return 0;
+    
+}
---
+++ svk.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int a,b,c;
+    // cout<<"Enter the first number"<< endl;
+    // cin>> a;
+    // cout<<"Enter the second number"<< endl;
+    // cin>> b;
+    // cout<<"The sum is "<< a + b<< endl;
+    // cout<<"The sub is "<< a - b<< endl;
+    // cout<<"The mul is "<< a * b<< endl;
+    // cout<<"The div is "<<(float) a / b<< endl;
+    int age;
+    cout<<"Enter your age "<< endl;
+    cin>>age;
+    if (age>150)
+    cout<<"Invalid";
+    else if (age>=18)
+    {
+        cout<<"You can vote";
+        /* code */
+    }
+    else
+    cout<<"you can not vote ";
+    return 0;
+
+    
+
+    return 0;
+ 
+
+
+
+}
