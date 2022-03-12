20a53e1f2fded608d5fb8450d0c49a9d605d9287
shivkumar <sosshivam65@gmail.com>
2022-03-12 23:34:54 +0530

    drive

---
+++ Hello.cpp/ifelse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int age;
+printf("Enter your age :",age);
+scanf("%d",&age);
+if (age>90)
+{
+    printf("you can not drive a car \n",age);
+
+}
+else
+{
+ printf("you can drivr a car ");
+}
+
+}
