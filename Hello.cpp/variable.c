81b00e4825b174f607372f8d9e9117202c3d50c6
shivkumar <sosshivam65@gmail.com>
2022-03-12 23:27:52 +0530

    practic

---
+++ Hello.cpp/variable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>>
+
+int main()
+{
+    int a,b,c;
+    printf("Enter the value of a is %d\n",a);
+    scanf("%d",&a);
+    printf("Enter the value of b is %d\n",b);
+    scanf("%d",&b);
+    c=a*b;
+    printf("The sum of is %d\n",c);
+    return 0;
+}
---
+++ shivam/practice.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+ printf("The value of 6+5 is %d\n",6+5);
+ printf("The value of 6+5.6 is %f\n",6+5.6);
+ return 0;
+
+}
