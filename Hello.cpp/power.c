694f1061a82369717e9da4379d1409d74b86ec6b
shivkumar <sosshivam65@gmail.com>
2022-03-12 23:25:15 +0530

    practice session

---
+++ Hello.cpp/power.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    // int a=3^3;
+    // printf("%d\n",a);
+    // float d=3.0/8-2;
+    // printf("%f\n",d);
+    int num1,num2;
+    // printf("Enter the num :");
+    // scanf("%d",&num);
+    // printf("Test divisivilty %d :\n",num%97);
+    printf("Enter a number :",num1);
+    scanf("%d",&num1);
+    if (num1%2==0){
+    printf(" %d is even num \n",num1);
+
+}
+       else{
+           printf(" %d odd num \n",num1);
+
+       }
+    
+
+    return 0;
+}
---
+++ Hello.cpp/vari.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+   int main()
+{
+    // int a,b,c;
+    // printf("Enter the value of a is :\n");
+    // scanf("%d",&a);
+    // printf("Enter the value of b is :\n");
+    // scanf("%d",&b);
+    // c=a+b;
+    // printf("The value of c is :%d\n",c);
+    // printf(" 5 divide by 2 leaves reaminde %d\n",5%2);
+    printf("the value 2 to the pow is 2 : %f\n",pow(2,2));
+     
+    return 0;
+}
