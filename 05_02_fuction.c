364740877109ab44e2fafb9425f7c945f27fdab0
shivkumar <sosshivam65@gmail.com>
2022-03-17 13:54:20 +0530

    practice

---
+++ 05_02_fuction.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void goodMorining();
+void goodAfternoon();
+void goodNight();
+
+
+int main()
+{
+    goodAfternoon();
+     goodMorining();
+     goodNight();
+    
+    return 0;
+}
+
+void goodMorining()
+{
+    printf("GoodMorning shiv kumar\n");
+}
+void goodAfternoon(){
+    printf("GoodAfter noon shiv kumar\n");
+
+}
+void goodNight()
+{
+    printf("GoodNight shiv kumar\n");
+}
