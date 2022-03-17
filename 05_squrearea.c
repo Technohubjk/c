a1d9bdac7faff6a15b48cf4f72682d22e5aefe9b
shivkumar <sosshivam65@gmail.com>
2022-03-17 13:53:50 +0530

    practicebaba.c

---
+++ 05_squrearea.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int side;
+    printf("Enter the value of side\n",side);
+    scanf("%d",&side);
+    printf("the vlaue of area is %f\n",pow(side,2));
+    return 0;
+}
