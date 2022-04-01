0bb4ceaf72b97db855090b27a3b141e86165f8a8
shivkumar <sosshivam65@gmail.com>
2022-04-01 18:31:46 +0530

    Array pass with pointer

---
+++ Array/08_Array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float f=3.14;
+    float *ptr=&f;
+    printf("The value of ptr is %u\n",ptr);
+    ptr=ptr+1;
+    // ptr=ptr+1;
+     printf("The value of ptr is %u\n",ptr);
+
+     return 0 ;  
+}  
+
+ 
