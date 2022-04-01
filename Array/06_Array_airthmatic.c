e7bf66177587584a5e8b5cd2518447f3170ca70a
shivkumar <sosshivam65@gmail.com>
2022-04-01 18:00:05 +0530

    Array

---
+++ Array/06_Array_airthmatic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i=34;
+    int *ptr=&i;
+    printf("The value of ptr is %u\n",ptr);
+    ptr++;
+     printf("The value of ptr is %u\n",ptr);
+     ptr++;
+      printf("The value of ptr is %u\n",ptr);
+      ptr++;
+       printf("The value of ptr is %u\n",ptr);
+       ptr++;
+
+}
---
+++ Array/07_Array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char i=34;
+    char *ptr=&i;
+    printf("The value of ptr is %u\n",ptr);
+    ptr++;
+     printf("The value of ptr is %u\n",ptr);
+     ptr++;
+      printf("The value of ptr is %u\n",ptr);
+      ptr++;
+       printf("The value of ptr is %u\n",ptr);
+       ptr++;
