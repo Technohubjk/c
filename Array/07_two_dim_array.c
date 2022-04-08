00ce049b50f8806842bafeefb4bfebdd53d6b278
shivkumar <sosshivam65@gmail.com>
2022-04-08 20:04:06 +0530

    Array

---
+++ Array/07_two_dim_array.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int A[3][3],B[3][3],C[3][3];
+    {
+    printf("Enter the 9 numbers of first matrix\n",A[3][3]);
+    for(int i=0;i<=2;i++)
+
+    for(int j=0;j<=2;j++)
+    scanf("%d",&A[i][j]);
+    }
+    {
+        printf("Enter the 9 numbers of second matrix\n",B[3][3]);
+        for(int i=0;i<=2;i++)
+
+    for(int j=0;j<=2;j++)
+    scanf("%d",&A[i][j]);
+    }
+    for(int i=0;i<=2;i++)
+    {
+        for (int j=0;j<=2;j++)
+        {
+            C[i][j]=A[i][j]+B[i][j];
+            printf("%d",C[i][j]);
+        
+        }
+        printf("\n");
+
+    }
+    return 0;
+
+
+
+}
