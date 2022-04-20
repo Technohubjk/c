467dbb9bf6f61ac5447f01a9a3c61f9ef1684e98
shivkumar <sosshivam65@gmail.com>
2022-04-20 11:48:38 +0530

    Add two number

---
+++ C Language practice serise/addtwonumber.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+ // namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter first number a\n :";
+    cin>>a;
+    cout<<"Enter second number b\n";
+    cin>>b;
+    c=a+b;
+    cout<<"\nAddition is : "<<c;
+    return 0;
+
+
+}
