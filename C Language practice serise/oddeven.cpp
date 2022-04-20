2765991d787a223cd9eaf4e186a531d37c006070
shivkumar <sosshivam65@gmail.com>
2022-04-20 12:00:53 +0530

    odd and even number

---
+++ C Language practice serise/oddeven.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cout<<"Enter a number :";
+    cin>>n;
+    if (n%2==0)
+    {
+        cout<<"this number is even number \n";
+
+    }
+    else{
+        cout<<"This number is odd numbrtr \n";
+
+    }
+}
