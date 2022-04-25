070ab1ebfeba6bc3becc854bf769f953078271cd
shivkumar <sosshivam65@gmail.com>
2022-04-25 17:02:28 +0530

    AA

---
+++ C Language practice serise/Area.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int  main()
+{
+    float area,radius;
+    cout<<"Enter Radius"<<endl;
+    cin>>radius;
+    area=3.14*radius*radius;
+    printf("Area : %6.2f",area);
+     return 0;
+
+
+
+
+}
